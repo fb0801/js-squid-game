97e2dfaa65fb29388a2a99cc017737c71d76b5fe
fb0801 <fbhatti0801@gmail.com>
2023-08-09 16:08:59 +0100

    creating tracks 3

--- js/main.js
+++ js/main.js
@@ -17,13 +17,14 @@ scene.add( light );
 const start_position = 6
 const end_position = -start_position
 
-function createCube(size, positionX, rotX = 0){
+function createCube(size, positionX, rotY = 0, color= 0xfbc851){
 	const geometry = new THREE.BoxGeometry(size.w, size.h, size.d);
-	const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+	const material = new THREE.MeshBasicMaterial( { color: color } );
 	const cube = new THREE.Mesh( geometry, material );
 		 cube.position.x = positionX; 
-		  cube.rotation.x = rotX;
+		  cube.rotation.y = rotY;
 	scene.add( cube );
+	return cube;
 }
 
 camera.position.z = 5;
@@ -52,9 +53,11 @@ class Doll{
 }
 
 function createTrack(){
+createCube({w: start_position * 2 + .21, h: 1.5, d: 1}, 0, 0, 0xe5a716).position.z = -1
 createCube({w: .2, h: 1.5, d: 1}, start_position, -.4)
 createCube({w: .2, h: 1.5, d: 1}, end_position, .4)
 }
+
 createTrack()
 
 let doll = new Doll();
